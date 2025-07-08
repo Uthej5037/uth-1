82b2f24bcbfa9156886d613160d6341a74968533
Uthej <uthejreddy.avula2000@gmail.com>
2025-07-08 14:28:05 -0500

    add this to git

---
+++ uth.java
@@ -0,0 +1,8 @@
+class Std1
+{
+	Public static void main(String args[])
+	{
+		System.out.pritnln(" I have a wonderful day");
+	}
+}
+
---
+++ uthej.java
@@ -0,0 +1,8 @@
+class Std
+{
+	Public static void main(String args[])
+	{
+		System.out.println("Hello World");
+	}
+}
+
